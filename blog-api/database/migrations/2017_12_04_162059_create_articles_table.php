954c17bf430f207a869fdb70ea4ac2fddde1849a
Daniel Sidabutar <aguspermadi94@gmail.com>
2017-12-04 23:43:25 +0700

    Adding migration article and user

---
+++ blog-api/database/migrations/2017_12_04_162059_create_articles_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateArticlesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title', 80);
+            $table->longText('content');
+            $table->foreign('author_id')->references('id')->on('user');
+            $table->timestamps();
+
+            // -- 
+            $table->increments('id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('articles');
+    }
+}
